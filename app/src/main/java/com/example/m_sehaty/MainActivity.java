7d75bd0df4052474038aa87824833ba1cb769574
asmaeyadaoui <asmaestudent12@gmail.com>
2022-04-12 17:05:39 +0000

    Updated gradle file + Main Activity

--- app/src/main/java/com/example/m_sehaty/MainActivity.java
+++ app/src/main/java/com/example/m_sehaty/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.m_sehaty;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class MainActivity extends AppCompatActivity{
+
+    private FirebaseAuth mAuth;
+    private Button CreateAccount;
+    private TextView LogIn;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        LogIn=findViewById(R.id.login_text);
+        LogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SignInActivity.class);
+                startActivity(intent);
+
+            }
+        });
+        CreateAccount=findViewById(R.id.getstartedbtn);
+        CreateAccount.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this,SignUpActivity.class);
+                startActivity(intent);
+
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/m_sehaty/User.java
@@ -0,0 +1,18 @@
+package com.example.m_sehaty;
+
+public class User {
+    public String firstName, lastName, phoneNumber, email, password;
+
+    public User(){
+    }
+
+    public User(String firstName, String lastName, String phoneNumber, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+
+    }
+
+
+}
