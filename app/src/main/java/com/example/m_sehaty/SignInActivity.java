b1b685c7680ec2cd1f421d551177d92eb35c555b
Salah Eddine Zemmouri <zemm.salaheddine@gmail.com>
2022-05-14 02:43:24 +0100

    fixed bug

--- app/src/main/java/com/example/m_sehaty/SignInActivity.java
+++ app/src/main/java/com/example/m_sehaty/SignInActivity.java
@@ -11,58 +11,43 @@ import android.widget.Toast;
 import com.abdsoft.med_dose.db.UserDAO;
 
 public class SignInActivity extends AppCompatActivity {
-    EditText firstname, lastname, email, phone, password, repassword;
-    Button signup , redirectToSignIn;
+    EditText email, password;
+    Button signin , redirectToSignUp;
 
     UserDAO DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
+        setContentView(R.layout.activity_sign_in);
 
-        firstname = (EditText) findViewById(R.id.sup_firstname);
-        lastname = (EditText) findViewById(R.id.sup_lastname);
-        email = (EditText) findViewById(R.id.sup_email);
-        phone = (EditText) findViewById(R.id.sup_number);
-        password = (EditText) findViewById(R.id.sup_password);
-        repassword = (EditText) findViewById(R.id.sup_confirmpassword);
-
-        DB = new com.abdsoft.med_dose.db.UserDAO(getApplicationContext());
-        signup = (Button) findViewById(R.id.signupbtn);
-        redirectToSignIn = (Button) findViewById(R.id.sup_already);
+        email = (EditText) findViewById(R.id.sin_email);
+        password = (EditText) findViewById(R.id.sin_password);
+        DB = new UserDAO(getApplicationContext());
+        signin = (Button) findViewById(R.id.signinbtn);
+        redirectToSignUp = (Button) findViewById(R.id.sin_dont_have);
 
 
-        signup.setOnClickListener( v -> {
-            String first_input = firstname.getText().toString();
-            String last_input = lastname.getText().toString();
-            String pass_input = password.getText().toString();
+        signin.setOnClickListener( v -> {
             String email_input = email.getText().toString();
-            String phone_input = phone.getText().toString();
-            String repass_input = repassword.getText().toString();
+            String pass_input = password.getText().toString();
 
-            if(first_input.equals("") || last_input.equals("") || pass_input.equals("") || email_input.equals("") || phone_input.equals("") || repass_input.equals(""))
-                Toast.makeText(SignUpActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
-            else if(!pass_input.equals(repass_input)){
-                Toast.makeText(SignUpActivity.this, "Password do not match", Toast.LENGTH_LONG).show();
-            }else{
-                if(DB.checkEmail(email_input)){
-                    Toast.makeText(SignUpActivity.this, "Email already registered", Toast.LENGTH_SHORT).show();
+            if(email_input.equals("") || pass_input.equals(""))
+                Toast.makeText(SignInActivity.this, "Please enter all the fields", Toast.LENGTH_SHORT).show();
+            else{
+                boolean checkuserpass = DB.checkEmailAndPassword(email_input, pass_input);
+                if(checkuserpass){
+                    Toast.makeText(SignInActivity.this, "Sign in successfull", Toast.LENGTH_SHORT).show();
+                    Intent intent  = new Intent(getApplicationContext(), com.abdsoft.med_dose.HomeActivity.class);
+                    startActivity(intent);
                 }else{
-                    boolean newUser = DB.registerNewUser(first_input, last_input, email_input, pass_input, phone_input);
-                    if(newUser){
-                        Toast.makeText(SignUpActivity.this, "Sign up successfull", Toast.LENGTH_SHORT).show();
-                        Intent intent  = new Intent(getApplicationContext(), com.abdsoft.med_dose.HomeActivity.class);
-                        startActivity(intent);
-                    }else{
-                        Toast.makeText(SignUpActivity.this, "Sign up failed", Toast.LENGTH_SHORT).show();
-                    }
+                    Toast.makeText(SignInActivity.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
                 }
             }
         });
 
-        redirectToSignIn.setOnClickListener( v -> {
-            Intent intent = new Intent(getApplicationContext(),SignInActivity.class);
+        redirectToSignUp.setOnClickListener( v -> {
+            Intent intent = new Intent(getApplicationContext(),SignUpActivity.class);
             startActivity(intent);
         });
     }
